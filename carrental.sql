1dc9a4062fa2d3a723ad25fb6f9aea86dbec6bcd
Swastik-Mantry <f20191019@pilani.bits-pilani.ac.in>
2022-04-09 13:26:25 +0530

    Changed ordering of tables

--- carrental.sql
+++ carrental.sql
@@ -1,7 +1,7 @@
 -- CREATE DATABASE carrentalsystem;
 -- Reservation, Car, Address --> Swastik
 -- Person, Waitlist, Model type --> Shreyas
---CREATE DATABASE carrentalsystem;
+-- CREATE DATABASE carrentalsystem;
 
 USE carrentalsystem;
 
@@ -21,9 +21,17 @@ DROP TABLE person;
 DROP TABLE person;
 DROP TABLE person;
 
-
+CREATE TABLE IF NOT EXISTS address(
+    addressID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    street VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    country VARCHAR(100),
+    zip INT UNSIGNED NOT NULL,
+    CONSTRAINT pk_a PRIMARY KEY(addressID)
+);
 CREATE TABLE IF NOT EXISTS person(
-    userID UNSIGNED INT NOT NULL AUTO_INCREMENT,
+    userID  INT UNSIGNED NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(50) NOT NULL DEFAULT '',
     lastName VARCHAR(50) NOT NULL DEFAULT '',
     email VARCHAR(50) NOT NULL,
@@ -31,37 +39,33 @@ CREATE TABLE IF NOT EXISTS person(
     userName VARCHAR(30) NOT NULL,  
     pwd VARCHAR(225) NOT NULL,
     gender ENUM('M','F','Other'),
-    addressID UNSIGNED INT NOT NULL,
+    addressID INT UNSIGNED NOT NULL,
     CONSTRAINT pk_p PRIMARY KEY(userID),
-    CONSTRAINT fk_p_a FOREIGN KEY(addressID) REFERENCES address(addressID),
+    CONSTRAINT fk_p_a FOREIGN KEY(addressID) REFERENCES address(addressID)
     );
 
 CREATE TABLE IF NOT EXISTS modelType(
     modelID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL DEFAULT '',
     description VARCHAR(100) DEFAULT '',
-    INT rate_by_hour NOT NULL,
-    INT rate_by_km NOT NULL,
-    CONSTRAINT pk_m PRIMARY KEY(modelID),
-    
-    );
+    rate_by_hour INT UNSIGNED NOT NULL,
+    rate_by_km INT UNSIGNED NOT NULL,
+    CONSTRAINT pk_m PRIMARY KEY(modelID)
+);
 
     CREATE TABLE IF NOT EXISTS waitlist(
     waitID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    modelID INT UNSIGNED NOT NULL
+    userID INT UNSIGNED NOT NULL,
+    modelID INT UNSIGNED NOT NULL,
+    timein timestamp NOT NULL,
+    timeout timestamp NOT NULL,
     
     CONSTRAINT pk_w PRIMARY KEY(waitID),
-    CONSTRAINT fk_w_p FOREIGN KEY(userID) REFERENCES person,
-    CONSTRAINT fk_w_m FOREIGN KEY(modelID) REFERENCES modelType,);
-
-CREATE TABLE IF NOT EXISTS address(
-    addressID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    street VARCHAR(100),
-    city VARCHAR(100),
-    state VARCHAR(100),
-    country VARCHAR(100),
-    zip INT UNSIGNED NOT NULL
+    CONSTRAINT fk_w_p FOREIGN KEY(userID) REFERENCES person(userID),
+    CONSTRAINT fk_w_m FOREIGN KEY(modelID) REFERENCES modelType(modelID)
 );
+
+
 CREATE TABLE IF NOT EXISTS car(
     carID int UNSIGNED NOT NULL AUTO_INCREMENT,
     modelID int UNSIGNED NOT NULL,
@@ -69,9 +73,9 @@ CREATE TABLE IF NOT EXISTS car(
     carimg VARCHAR(200),
     CONSTRAINT pk_c PRIMARY KEY(carID),
     CONSTRAINT fk_c_m FOREIGN KEY(modelID) REFERENCES modelType(modelID)
-        on delete set NULL,
+        on delete set ,
     CONSTRAINT fk_c_p FOREIGN KEY(ownerID) REFERENCES person(userID)
-        on delete cascade,
+        on delete cascade
 );
 CREATE TABLE IF NOT EXISTS reservation(
     rID int UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -81,7 +85,7 @@ CREATE TABLE IF NOT EXISTS reservation(
     value numeric(6,2) not null,
     timein TIMESTAMP not null,
     timeout timestamp not null,
-    CONSTAINT pk_r PRIMARY KEY(rID),
+    CONSTRAINT pk_r PRIMARY KEY(rID),
     CONSTRAINT fk_r_p FOREIGN KEY(userID) REFERENCES person(userID)
         on delete cascade,
     CONSTRAINT fk_r_c FOREIGN KEY(carID) REFERENCES car(carID)
